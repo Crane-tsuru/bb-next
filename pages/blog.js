520f78490755a9ce2c9f4bfd2d95189283e75a33
Cacao1522 <kaorukato2003@gmail.com>
2023-03-02 13:40:31 +0900

    返信ボタンの処理変更、ブログの戻るリンク追加

--- pages/blog.js
+++ pages/blog.js
@@ -1,16 +1,22 @@
+import Head from "next/head";
 import Link from 'next/link';
 import { client } from "../libs/client";
+import classes from '../components/BlogList.module.scss';
 
 export default function Blog({ blog }) {
   return (
     <div>
-        <Link href="/">
+       <Head>
+        <title>Web班のブログ</title>
+        <meta name="description" content="名工大プログラミング部C0deのWeb班のブログです。"  />
+      </Head>
+        <Link className={classes.link} href="/">
         掲示板へ
       </Link>
       <ul>
         {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>　{new Date(blog.publishedAt).toLocaleDateString()}
+          <li className={classes.card} key={blog.id}>
+            <Link className={classes.link} href={`/blog/${blog.id}`}>{blog.title}</Link>　{new Date(blog.publishedAt).toLocaleDateString()}
           </li>
         ))}
       </ul>
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,21 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "../components/fire";
 import Head from "next/head";
-import Link from 'next/link';
-import classes from "../styles/Home.module.css"
+import Link from "next/link";
+import classes from "../styles/Home.module.css";
 
 const db = firebase.firestore();
 
 export default function Home() {
   const mydata = [];
-  const mydata2 = [];
-
+  let replyArray = [];
+  let index = -1;
+  let replyCount = 0;
   const [data, setData] = useState(mydata);
-  const [data2, setData2] = useState(mydata2);
   const [message, setMessage] = useState("");
   const [name, setName] = useState("");
   const [text, setText] = useState("");
+  const [count, setCount] = useState(0);
   const [replyName, setReplyName] = useState("");
   const [replyText, setReplyText] = useState("");
   const [replyId, setReplyId] = useState("");
@@ -40,44 +41,40 @@ export default function Home() {
   //Firebaseに投稿文を追加.その後useEffect起動
   const addText = (e) => {
     if (name.length === 0 || text.length === 0) {
-      setMessage(
-        "未入力エラーです"
-      );
+      setMessage("未入力エラーです");
     } else {
       const ob = {
         name: name,
         text: text,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         id: db.collection("mydata").doc(),
-        reply: false,
+        index: index,
       };
-  
-      ob.id.set(ob).then(() => {     
-        setText("");//テキストボックスをクリア
+
+      ob.id.set(ob).then(() => {
+        setText(""); //テキストボックスをクリア
         setFlag(!flag);
         setMessage("投稿しました");
       });
     }
   };
 
-  //返信ボタンが押されたらreplyフィールドを反転、idを保存.その後useEffect起動
-  const ReplyBotton = (id, reply) => { 
-    if (reply) {
-      id.update({
-        reply: false,
-      }).then(() => {
-        setReplyFlag(false);
-        setFlag(!flag);
-      });
+  //返信ボタンが押されたらreplyFlagを反転、idを保存、押された投稿よりも上にある投稿の数を数える
+  const ReplyBotton = (id, Index) => {
+    if (replyFlag) {
+      setReplyFlag((prev) => !prev);
+      setReplyId(id);
     } else {
-      id.update({
-        reply: true,
-      }).then(() => {
-        setReplyFlag(true);
-        setFlag(!flag);
-      });
+      setReplyFlag((prev) => !prev);
+      setReplyId(id);
     }
-    setReplyId(id);
+
+    replyCount = 0;
+    for (let i = 0; i <= Index; i++) {
+      replyCount += replyArray[i];
+    }
+    index = Index;
+    setCount(index + replyCount + 1);
   };
 
   //Firebaseに返信文を追加
@@ -91,121 +88,101 @@ export default function Home() {
 
     ob.id
       .collection("replyText")
-      .add(ob) 
+      .add(ob)
       .then(() => {
-        setReplyText("");//テキストボックスをクリア
+        setReplyText(""); //テキストボックスをクリア
         setFlag(!flag);
         setMessage("返信しました");
       });
   };
 
   //初回とflagが変わったときに実行（表示の更新）
-  useEffect(() => {   
+  useEffect(() => {
     mydata.length = 0;
-    mydata2.length = 0;
-    let replyBotton = true;
 
     //mydataコレクションのドキュメントを取得.投稿時間で昇順にソート.
     db.collection("mydata")
       .orderBy("timestamp", "asc")
       .get()
-      .then(async(snapshot) => {
-        
-        for await(const document of snapshot.docs) {
+      .then(async (snapshot) => {
+        for await (const document of snapshot.docs) {
           const doc = document.data(); //docにフィールドの情報がまとめられたオブジェクトを代入
 
-          if (replyBotton) {
-            if (doc.reply) {
-              replyBotton = false; //replyフィールドがtrueだったら次の投稿からmydata2にpush
-              doc.id.update({reply: false})
-            }
-            mydata.push(
+          index += 1;
+
+          mydata.push(
+            <div>
               <div>
-                <div>
-                  名前:{doc.name}　
-                  {new Date(doc.timestamp?.toDate()).toLocaleString()}　
-                  <button onClick={() => ReplyBotton(doc.id, doc.reply)}>
-                    返信
-                  </button>
-                </div>
-                <div>{doc.text}</div>
+                名前:{doc.name}　
+                {new Date(doc.timestamp?.toDate()).toLocaleString()}　
+                <button onClick={() => ReplyBotton(doc.id, doc.index)}>
+                  返信
+                </button>
               </div>
-            );
-          } else {
-            mydata2.push(
-              <div>
-                <div>
-                  名前:{doc.name}　
-                  {new Date(doc.timestamp?.toDate()).toLocaleString()}　
-                  <button onClick={() => ReplyBotton(doc.id, doc.reply)}>
-                    返信
-                  </button>
-                </div>
-                <div>{doc.text}</div>
-              </div>
-            );
-          }
-        
+              <div>{doc.text}</div>
+            </div>
+          );
+
+          //replyTextコレクションのドキュメント(返信内容)を取得.投稿時間で昇順にソート.
           const snapshot2 = await doc.id
             .collection("replyText")
             .orderBy("timestamp", "asc")
             .get();
-            
-              snapshot2.forEach((document2) => {
-                const doc2 = document2.data();
 
-                if (replyBotton) {
-                  if (doc.reply) replyBotton = false;
-                  mydata.push(
-                    <div>
-                      <div>
-                        ＞＞名前:{doc2.name}　
-                        {new Date(doc2.timestamp?.toDate()).toLocaleString()}　
-                      </div>
-                      <div>　　{doc2.text}</div>
-                    </div>
-                  );
-                } else {
-                  mydata2.push(
-                    <div>
-                      <div>
-                        ＞＞名前:{doc2.name}　
-                        {new Date(doc2.timestamp?.toDate()).toLocaleString()}　
-                      </div>
-                      <div>　　{doc2.text}</div>
-                    </div>
-                  );
-                }
-              }); //forEach(document2)
+          snapshot2.forEach((document2) => {
+            const doc2 = document2.data();
 
-              setData(mydata); //mydataをdataに代入
-              setData2(mydata2); //mydataをdataに代入
-              setMessage(
-                "質問などあればお書きください"
-              );
-              setNum((prev) => prev + 1)
+            mydata.push(
+              <div>
+                <div>
+                  ＞＞名前:{doc2.name}　
+                  {new Date(doc2.timestamp?.toDate()).toLocaleString()}　
+                </div>
+                <div>　　{doc2.text}</div>
+              </div>
+            );
 
-        }; //forEach(document)
+            if (replyArray[index]) {
+              replyArray[index] += 1;
+            } else {
+              replyArray[index] = 1;
+            }
+          }); //forEach(document2)
+
+          setData(mydata); //mydataをdataに代入
+          setMessage("質問などあればお書きください");
+          setNum((prev) => prev + 1);
+        } //forEach(document)
       }); //snapshot
-      
   }, [flag]);
 
   return (
     <div>
       <Head>
         <title>C0deWeb班掲示板</title>
-        <meta name="description" content="名工大プログラミング部C0deのWeb班が制作した質問掲示板です。"  />
+        <meta
+          name="description"
+          content="名工大プログラミング部C0deのWeb班が制作した質問掲示板です。"
+        />
       </Head>
       <h2 className={classes.title}>C0de Web班掲示板 {message}</h2>
-      <Link href="/blog">
+      <Link className={classes.link} href="/blog">
         Web班のブログへ
       </Link>
 
-      <table>
-        <thead>
-          <tr>{data}</tr>
-        </thead>
-      </table>
+      {replyFlag ? (
+        <table>
+          <thead>
+            <tr>{data.slice(0, count)}</tr>
+          </thead>
+        </table>
+      ) : (
+        <table>
+          <thead>
+            <tr>{data}</tr>
+          </thead>
+        </table>
+      )}
 
       {replyFlag && (
         <div>
@@ -220,17 +197,24 @@ export default function Home() {
           </p>
           <p>
             返信内容　
-            <textarea className={classes.textarea} value={text} onChange={onChangeReplyText} rows="4" cols="50" maxLength="400"/>
+            <textarea
+              className={classes.textarea}
+              value={text}
+              onChange={onChangeReplyText}
+              rows="4"
+              cols="50"
+              maxLength="400"
+            />
           </p>
           <button onClick={addReplyText}>返信する</button>
+
+          <table>
+            <thead>
+              <tr>{data.slice(count, data.length)}</tr>
+            </thead>
+          </table>
         </div>
       )}
-
-      <table>
-        <thead>
-          <tr>{data2}</tr>
-        </thead>
-      </table>
 
       <p>
         お名前　
@@ -243,7 +227,14 @@ export default function Home() {
       </p>
       <p>
         投稿内容　
-        <textarea className={classes.textarea} value={text} onChange={onChangeText} rows="4" cols="50" maxLength="400"/>
+        <textarea
+          className={classes.textarea}
+          value={text}
+          onChange={onChangeText}
+          rows="4"
+          cols="50"
+          maxLength="400"
+        />
       </p>
       <button onClick={addText}>投稿する</button>
     </div>
